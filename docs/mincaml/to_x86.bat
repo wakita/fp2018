4cacfa621e292f8a9e2d4463e70d381a826027eb
Ken Wakita <wakita@is.titech.ac.jp>
2018-10-11 16:18:50 +0900

    day4-mincaml-to_x86.bat

---
+++ docs/mincaml/to_x86.bat
@@ -0,0 +1,8 @@
+copy x86\*.ml .
+copy x86\*.mli .
+copy x86\libmincaml.S .
+copy test\x86\manyargs.ml test
+copy test\x86\toomanyargs.ml test
+copy shootout\x86\Makefile shootout
+copy bytemark\x86\Makefile bytemark
+copy min-rt\x86\globals.s min-rt
